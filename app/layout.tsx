431d334508c32b50930db2c2d7b9e3a1c32f4e97
Dustin Miller <141868171+dustinmiller412@users.noreply.github.com>
2024-11-19 20:30:08 -0500

    Added dnd branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./global.css"
 
 export const metadata = {
     title: "DnDGPT",
-    description: "The place to go for DnD 5e questions"
+    description: "The place to go for DnD 5e questions!"
 }
 
 const RootLayout = ({children}) => {
